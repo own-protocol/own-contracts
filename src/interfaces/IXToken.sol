65bcc0fb1ae643b68afe760cbb23c4beaa965ca6
eagle <aparokshambhargav@gmail.com>
2024-12-20 17:07:40 +0530

    feat: update XToken interface

--- src/interfaces/IXToken.sol
+++ src/interfaces/IXToken.sol
@@ -6,7 +6,7 @@ import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 import {IScaledBalanceToken} from './IScaledBalanceToken.sol';
 import {IInitializableXToken} from './IInitializableXToken.sol';
 
-interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
+interface IXToken is IERC20, IScaledBalanceToken, IInitializableXToken {
   /**
    * @dev Emitted after the mint action
    * @param from The address performing the mint
@@ -16,7 +16,7 @@ interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   event Mint(address indexed from, uint256 value, uint256 index);
 
   /**
-   * @dev Mints `amount` aTokens to `user`
+   * @dev Mints `amount` xTokens to `user`
    * @param user The address receiving the minted tokens
    * @param amount The amount of tokens getting minted
    * @param index The new liquidity index of the reserve
@@ -29,8 +29,8 @@ interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   ) external returns (bool);
 
   /**
-   * @dev Emitted after aTokens are burned
-   * @param from The owner of the aTokens, getting them burned
+   * @dev Emitted after xTokens are burned
+   * @param from The owner of the xTokens, getting them burned
    * @param target The address that will receive the underlying
    * @param value The amount being burned
    * @param index The new liquidity index of the reserve
@@ -47,8 +47,8 @@ interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   event BalanceTransfer(address indexed from, address indexed to, uint256 value, uint256 index);
 
   /**
-   * @dev Burns aTokens from `user` and sends the equivalent amount of underlying to `receiverOfUnderlying`
-   * @param user The owner of the aTokens, getting them burned
+   * @dev Burns xTokens from `user` and sends the equivalent amount of underlying to `receiverOfUnderlying`
+   * @param user The owner of the xTokens, getting them burned
    * @param receiverOfUnderlying The address that will receive the underlying
    * @param amount The amount being burned
    * @param index The new liquidity index of the reserve
@@ -61,15 +61,15 @@ interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   ) external;
 
   /**
-   * @dev Mints aTokens to the reserve treasury
+   * @dev Mints xTokens to the reserve treasury
    * @param amount The amount of tokens getting minted
    * @param index The new liquidity index of the reserve
    */
   function mintToTreasury(uint256 amount, uint256 index) external;
 
   /**
-   * @dev Transfers aTokens in the event of a borrow being liquidated, in case the liquidators reclaims the aToken
-   * @param from The address getting liquidated, current owner of the aTokens
+   * @dev Transfers xTokens in the event of a borrow being liquidated, in case the liquidators reclaims the xToken
+   * @param from The address getting liquidated, current owner of the xTokens
    * @param to The recipient
    * @param value The amount of tokens getting transferred
    **/
@@ -89,14 +89,14 @@ interface IXToken is IERC20, IScaledBalanceToken, IInitializableAToken {
   function transferUnderlyingTo(address user, uint256 amount) external returns (uint256);
 
   /**
-   * @dev Invoked to execute actions on the aToken side after a repayment.
+   * @dev Invoked to execute actions on the xToken side after a repayment.
    * @param user The user executing the repayment
    * @param amount The amount getting repaid
    **/
   function handleRepayment(address user, uint256 amount) external;
 
   /**
-   * @dev Returns the address of the underlying asset of this aToken (E.g. WETH for aWETH)
+   * @dev Returns the address of the underlying asset of this xToken.
    **/
   function UNDERLYING_ASSET_ADDRESS() external view returns (address);
 }
