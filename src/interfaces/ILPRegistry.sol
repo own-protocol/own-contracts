446faaac41d7ff817a8b78eb38095a382b53dbcd
eagle <aparokshambhargav@gmail.com>
2025-01-09 12:34:16 +0530

    feat: add lpLiquidity in lp registry

--- src/interfaces/ILPRegistry.sol
+++ src/interfaces/ILPRegistry.sol
@@ -4,21 +4,29 @@
 pragma solidity ^0.8.20;
 
 interface ILPRegistry {
-    event LPRegistered(address indexed pool, address indexed lp);
+    event LPRegistered(address indexed pool, address indexed lp, uint256 liquidityAmount);
     event LPRemoved(address indexed pool, address indexed lp);
     event PoolAdded(address indexed pool);
     event PoolRemoved(address indexed pool);
+    event LiquidityIncreased(address indexed pool, address indexed lp, uint256 amount);
+    event LiquidityDecreased(address indexed pool, address indexed lp, uint256 amount);
 
     error NotAuthorized();
     error PoolNotFound();
     error AlreadyRegistered();
     error NotRegistered();
+    error InsufficientLiquidity();
+    error InvalidAmount();
 
-    function registerLP(address pool, address lp) external;
+    function registerLP(address pool, address lp, uint256 liquidityAmount) external;
     function removeLP(address pool, address lp) external;
     function addPool(address pool) external;
     function removePool(address pool) external;
+    function increaseLiquidity(address pool, uint256 amount) external;
+    function decreaseLiquidity(address pool, uint256 amount) external;
     function isLP(address pool, address lp) external view returns (bool);
     function getLPCount(address pool) external view returns (uint256);
     function isValidPool(address pool) external view returns (bool);
+    function getLPLiquidity(address pool, address lp) external view returns (uint256);
+    function getTotalLPLiquidity(address pool) external view returns (uint256);
 }
--- src/protocol/LPRegistry.sol
+++ src/protocol/LPRegistry.sol
@@ -10,11 +10,20 @@ contract LPRegistry is ILPRegistry, Ownable {
     mapping(address => bool) public validPools;
     mapping(address => mapping(address => bool)) public poolLPs;
     mapping(address => uint256) public poolLPCount;
+    
+    // State variables for liquidity amounts
+    mapping(address => mapping(address => uint256)) public lpLiquidityAmount;
+    mapping(address => uint256) public totalLPLiquidity;
 
     constructor() Ownable(msg.sender) {}
 
     modifier onlyValidPool(address pool) {
         if (!validPools[pool]) revert PoolNotFound();
+        _;
+    }
+
+    modifier onlyRegisteredLP(address pool, address lp) {
+        if (!poolLPs[pool][lp]) revert NotRegistered();
         _;
     }
 
@@ -26,22 +35,50 @@ contract LPRegistry is ILPRegistry, Ownable {
 
     function removePool(address pool) external onlyOwner {
         if (!validPools[pool]) revert PoolNotFound();
+        // Only allow removal if no liquidity remains
+        if (totalLPLiquidity[pool] > 0) revert("Pool has active liquidity");
         validPools[pool] = false;
         emit PoolRemoved(pool);
     }
 
-    function registerLP(address pool, address lp) external onlyOwner onlyValidPool(pool) {
+    function registerLP(address pool, address lp, uint256 liquidityAmount) external onlyOwner onlyValidPool(pool) {
         if (poolLPs[pool][lp]) revert AlreadyRegistered();
+        if (liquidityAmount == 0) revert InvalidAmount();
+        
         poolLPs[pool][lp] = true;
         poolLPCount[pool]++;
-        emit LPRegistered(pool, lp);
+        lpLiquidityAmount[pool][lp] = liquidityAmount;
+        totalLPLiquidity[pool] += liquidityAmount;
+        
+        emit LPRegistered(pool, lp, liquidityAmount);
     }
 
-    function removeLP(address pool, address lp) external onlyOwner onlyValidPool(pool) {
-        if (!poolLPs[pool][lp]) revert NotRegistered();
+    function removeLP(address pool, address lp) external onlyOwner onlyValidPool(pool) onlyRegisteredLP(pool, lp) {
+        // Only allow removal if no liquidity remains
+        if (lpLiquidityAmount[pool][lp] > 0) revert("LP has active liquidity");
+        
         poolLPs[pool][lp] = false;
         poolLPCount[pool]--;
         emit LPRemoved(pool, lp);
+    }
+
+    function increaseLiquidity(address pool, uint256 amount) external onlyValidPool(pool) onlyRegisteredLP(pool, msg.sender) {
+        if (amount == 0) revert InvalidAmount();
+        
+        lpLiquidityAmount[pool][msg.sender] += amount;
+        totalLPLiquidity[pool] += amount;
+        
+        emit LiquidityIncreased(pool, msg.sender, lpLiquidityAmount[pool][msg.sender]);
+    }
+
+    function decreaseLiquidity(address pool, uint256 amount) external onlyValidPool(pool) onlyRegisteredLP(pool, msg.sender) {
+        if (amount == 0) revert InvalidAmount();
+        if (amount > lpLiquidityAmount[pool][msg.sender]) revert InsufficientLiquidity();
+        
+        lpLiquidityAmount[pool][msg.sender] -= amount;
+        totalLPLiquidity[pool] -= amount;
+        
+        emit LiquidityDecreased(pool, msg.sender, amount);
     }
 
     function isLP(address pool, address lp) external view returns (bool) {
@@ -54,5 +91,13 @@ contract LPRegistry is ILPRegistry, Ownable {
 
     function isValidPool(address pool) external view returns (bool) {
         return validPools[pool];
+    }
+
+    function getLPLiquidity(address pool, address lp) external view returns (uint256) {
+        return lpLiquidityAmount[pool][lp];
+    }
+
+    function getTotalLPLiquidity(address pool) external view returns (uint256) {
+        return totalLPLiquidity[pool];
     }
 }
