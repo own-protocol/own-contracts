03c6603926d815cec98511699ec0de4d805edb63
eagle <aparokshambhargav@gmail.com>
2025-01-10 19:15:10 +0530

    feat: update xToken contract

--- src/protocol/xToken.sol
+++ src/protocol/xToken.sol
@@ -23,12 +23,6 @@ contract xToken is IXToken, ERC20 {
 
     /// @notice Version identifier for the xToken implementation
     uint256 public constant XTOKEN_VERSION = 0x1;
-    
-    /**
-     * @notice Price scaling factor to convert cent prices to wei-equivalent precision
-     * @dev Since price is in cents (1/100 of dollar), we multiply by 1e16 to get to 1e18 precision
-     */
-    uint256 private constant PRICE_SCALING = 1e16;
 
     /// @notice Mapping of scaled balances for each account
     mapping(address => uint256) private _scaledBalances;
@@ -83,30 +77,7 @@ contract xToken is IXToken, ERC20 {
     function _convertToScaledAmount(uint256 amount) internal view returns (uint256) {
         uint256 price = oracle.assetPrice();
         if (price == 0) revert InvalidPrice();
-        return (amount * 1e18) / (price * PRICE_SCALING);
-    }
-
-    /**
-     * @notice Returns the current balance of an account in nominal terms
-     * @dev Converts the scaled balance to nominal terms using current asset price
-     * @param account The address of the account
-     * @return The nominal balance of the account
-     */
-    function balanceOf(address account) public view override(IERC20, ERC20) returns (uint256) {
-        uint256 price = oracle.assetPrice();
-        if (price == 0) revert InvalidPrice();
-        return (_scaledBalances[account] * (price * PRICE_SCALING)) / 1e18;
-    }
-
-    /**
-     * @notice Returns the total supply in nominal terms
-     * @dev Converts the total scaled supply to nominal terms using current asset price
-     * @return The total supply of tokens
-     */
-    function totalSupply() public view override(IERC20, ERC20) returns (uint256) {
-        uint256 price = oracle.assetPrice();
-        if (price == 0) revert InvalidPrice();
-        return (_totalScaledSupply * (price * PRICE_SCALING)) / 1e18;
+        return amount * price;
     }
 
     /**
@@ -119,6 +90,7 @@ contract xToken is IXToken, ERC20 {
         uint256 scaledAmount = _convertToScaledAmount(amount);
         _scaledBalances[account] += scaledAmount;
         _totalScaledSupply += scaledAmount;
+        _mint(account, amount);
         emit Mint(account, amount);
     }
 
@@ -129,55 +101,11 @@ contract xToken is IXToken, ERC20 {
      * @param amount The amount of tokens to burn (in nominal terms)
      */
     function burn(address account, uint256 amount) external onlyPool {
+        if (_balances[account] < amount) revert InsufficientBalance();
         uint256 scaledAmount = _convertToScaledAmount(amount);
-        if (_scaledBalances[account] < scaledAmount) revert InsufficientBalance();
         _scaledBalances[account] -= scaledAmount;
         _totalScaledSupply -= scaledAmount;
+        _burn(account, amount);
         emit Burn(account, amount);
-    }
-
-    /**
-     * @notice Transfers tokens to a recipient
-     * @param recipient The address receiving the tokens
-     * @param amount The amount of tokens to transfer (in nominal terms)
-     * @return success True if the transfer succeeded
-     */
-    function transfer(address recipient, uint256 amount) public override(ERC20, IERC20) returns (bool) {
-        if (recipient == address(0)) revert ZeroAddress();
-        uint256 scaledAmount = _convertToScaledAmount(amount);
-        if (_scaledBalances[msg.sender] < scaledAmount) revert InsufficientBalance();
-        
-        _scaledBalances[msg.sender] -= scaledAmount;
-        _scaledBalances[recipient] += scaledAmount;
-        
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    /**
-     * @notice Transfers tokens from one address to another using the allowance mechanism
-     * @param sender The address to transfer tokens from
-     * @param recipient The address receiving the tokens
-     * @param amount The amount of tokens to transfer (in nominal terms)
-     * @return success True if the transfer succeeded
-     */
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override(ERC20, IERC20) returns (bool) {
-        if (recipient == address(0)) revert ZeroAddress();
-        uint256 currentAllowance = allowance(sender, msg.sender);
-        if (currentAllowance < amount) revert InsufficientAllowance();
-
-        uint256 scaledAmount = _convertToScaledAmount(amount);
-        if (_scaledBalances[sender] < scaledAmount) revert InsufficientBalance();
-
-        _scaledBalances[sender] -= scaledAmount;
-        _scaledBalances[recipient] += scaledAmount;
-        _approve(sender, msg.sender, currentAllowance - amount);
-
-        emit Transfer(sender, recipient, amount);
-        return true;
     }
 }
