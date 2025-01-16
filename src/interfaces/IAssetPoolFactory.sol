370079e54550c4dd5570965cbab0cb37babafd4b
eagle <aparokshambhargav@gmail.com>
2025-01-16 18:47:12 +0530

    feat: add code comments to pool factory

--- src/interfaces/IAssetPoolFactory.sol
+++ src/interfaces/IAssetPoolFactory.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.20;
 
 import {ILPRegistry} from './ILPRegistry.sol';
 
+/**
+ * @title IPoolFactory
+ * @dev Interface defining the structure of the PoolFactory contract.
+ * Responsible for creating and managing asset pools for liquidity provisioning.
+ */
 interface IPoolFactory {
+    /**
+     * @dev Emitted when a new asset pool is created.
+     * @param pool Address of the newly created pool.
+     * @param assetSymbol Symbol representing the asset.
+     * @param depositToken Address of the token used for deposits.
+     * @param oracle Address of the oracle used for asset price feeds.
+     * @param cycleLength Duration of a single investment cycle in seconds.
+     * @param rebalancingPeriod Duration of the rebalancing period within a cycle in seconds.
+     */
     event PoolCreated(
         address indexed pool,
         string assetSymbol,
@@ -15,11 +29,32 @@ interface IPoolFactory {
         uint256 rebalancingPeriod
     );
 
+    /**
+     * @dev Reverts when provided parameters are invalid.
+     */
     error InvalidParams();
+
+    /**
+     * @dev Reverts when a zero address is provided where a valid address is required.
+     */
     error ZeroAddress();
 
+    /**
+     * @return ILPRegistry The address of the LP Registry contract.
+     */
     function lpRegistry() external view returns (ILPRegistry);
-    
+
+    /**
+     * @dev Creates a new asset pool with the specified parameters.
+     * @param assetSymbol Symbol of the asset.
+     * @param assetTokenName Name of the token representing the asset.
+     * @param assetTokenSymbol Symbol of the token representing the asset.
+     * @param depositToken Address of the token used for deposits.
+     * @param oracle Address of the oracle providing asset price feeds.
+     * @param cycleLength Length of each investment cycle in seconds.
+     * @param rebalancingPeriod Rebalancing period length within a cycle in seconds.
+     * @return address The address of the newly created asset pool.
+     */
     function createPool(
         string memory assetSymbol,
         string memory assetTokenName,
--- src/protocol/AssetPoolFactory.sol
+++ src/protocol/AssetPoolFactory.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: AGPL-3.0
 // author: bhargavaparoksham
 
@@ -9,14 +8,42 @@ import {ILPRegistry} from '../interfaces/ILPRegistry.sol';
 import {IPoolFactory} from '../interfaces/IAssetPoolFactory.sol';
 import {AssetPool} from './AssetPool.sol';
 
+/**
+ * @title PoolFactory
+ * @dev Implementation of the IPoolFactory interface.
+ * Responsible for creating and registering asset pools for liquidity provisioning.
+ */
 contract PoolFactory is IPoolFactory, Ownable {
+    /// @notice Reference to the LP Registry contract.
     ILPRegistry public immutable lpRegistry;
-    
+
+    /**
+     * @dev Constructor to initialize the PoolFactory contract.
+     * @param _lpRegistry Address of the LP Registry contract.
+     * Reverts if the address is zero.
+     */
     constructor(address _lpRegistry) Ownable(msg.sender) {
         if (_lpRegistry == address(0)) revert ZeroAddress();
         lpRegistry = ILPRegistry(_lpRegistry);
     }
 
+    /**
+     * @dev Creates a new asset pool with the given parameters.
+     * Only callable by the owner of the contract.
+     * Reverts if:
+     * - Any address parameter is zero.
+     * - `cyclePeriod` is zero.
+     * - `rebalancingPeriod` is greater than or equal to `cyclePeriod`.
+     * 
+     * @param assetSymbol Symbol of the asset.
+     * @param assetTokenName Name of the token representing the asset.
+     * @param assetTokenSymbol Symbol of the token representing the asset.
+     * @param depositToken Address of the token used for deposits.
+     * @param oracle Address of the oracle providing asset price feeds.
+     * @param cyclePeriod Length of each investment cycle in seconds.
+     * @param rebalancingPeriod Length of the rebalancing period within a cycle in seconds.
+     * @return address The address of the newly created asset pool.
+     */
     function createPool(
         string memory assetSymbol,
         string memory assetTokenName,
@@ -33,6 +60,7 @@ contract PoolFactory is IPoolFactory, Ownable {
             rebalancingPeriod >= cyclePeriod
         ) revert InvalidParams();
 
+        // Deploy a new AssetPool contract instance.
         AssetPool pool = new AssetPool(
             depositToken,
             assetTokenName,
@@ -44,8 +72,10 @@ contract PoolFactory is IPoolFactory, Ownable {
             msg.sender
         );
 
+        // Register the newly created pool in the LP Registry.
         lpRegistry.addPool(address(pool));
 
+        // Emit the PoolCreated event to notify listeners.
         emit PoolCreated(
             address(pool),
             assetSymbol,
